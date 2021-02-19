f426d83e4f0a3939f757d49582c71693ec1497c5
Juan Belieni <juanbelieni@gmail.com>
2021-02-19 18:51:50 -0300

    Learn how to enter data

---
+++ 11-entering-data/entering-data.r
@@ -0,0 +1,22 @@
+# Entering data
+
+# Assing 0 through 10
+(x1 <- 0:10)
+
+# Descending order
+(x2 <- 10:0)
+
+# With seq comand
+(x3 <- seq(10))
+(x4 <- seq(30, 0, by = -3))
+
+# With c commmand
+(x5 <- c(5, 2, 1, 6, 5, 3, 3))
+
+# With scan command
+(x6 <- scan())
+
+# With rep command
+(x7 <- rep(T, 5))
+(x8 <- rep(c(T, F), 5))
+(x9 <- rep(c(T, F), each = 5))
